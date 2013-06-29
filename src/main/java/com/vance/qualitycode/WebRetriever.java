1c2332c99f1ab8fada722b8fce56659d707c3583
Stephen Vance <steve@vance.com>
2013-06-29 17:31:34 -0400

    Populate project with 'mvn archetype:generate'
    Used com.github.igor-petruk.archetypes:maven-archetype-executable

---
+++ src/main/java/com/vance/qualitycode/WebRetriever.java
@@ -0,0 +1,8 @@
+package com.vance.qualitycode;
+
+public class WebRetriever 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/test/java/com/vance/qualitycode/WebRetrieverTest.java
@@ -0,0 +1,12 @@
+package com.vance.qualitycode;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class WebRetrieverTest 
+{
+    @Test
+    public void testWebRetriever()
+    {
+    }
+}
